8906d61eb35e1f7dd8fdbdb140dd1332c0a3e291
spw <pixbin@gmail.com>
2025-11-11 23:46:39 +0000

    added mantine styling

--- dash_app/app_20.py
+++ dash_app/app_20.py
@@ -2,6 +2,8 @@
 from dash import Dash, html, dash_table, dcc, callback, Output, Input
 import pandas as pd
 import plotly.express as px
+import dash_mantine_components as dmc
+import dash_ag_grid as dag
 
 # Incorporate data
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
@@ -10,18 +12,33 @@ df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapmi
 app = Dash()
 
 # App layout
-app.layout = [
-    html.Div(children='My First App with Data, Graph, and Controls')
-    ,html.Hr()
-    ,dcc.RadioItems(options=['pop', 'lifeExp', 'gdpPercap'], value='lifeExp', id='controls-and-radio-item') # callback input component
-    ,dash_table.DataTable(data=df.to_dict('records'), page_size=6) # type: ignore
-    ,dcc.Graph(figure={}, id='controls-and-graph') # callback output reviever. Empty dictionary/graph.
-]
+app.layout = dmc.MantineProvider(
+
+    dmc.Container([
+        dmc.Title("My First App with Data, Graph, and Controls", c="blue", order=3), # order is size of text
+
+        dmc.RadioGroup(
+           dmc.Group([dmc.Radio(i, value=i) for i in  ['pop', 'lifeExp', 'gdpPercap']]), # callback input component
+            id='my-dmc-radio-item',
+            value='lifeExp',
+            p="sm"
+        ),
+
+        dmc.SimpleGrid([
+            dag.AgGrid(
+                rowData=df.to_dict("records"),
+                columnDefs=[{"field": i} for i in df.columns],
+            ),
+
+            dcc.Graph(figure={}, id='graph-placeholder') # callback output reviever. Empty dictionary/graph.
+        ], cols={"base": 1, "md": 2}) # type: ignore
+    ], fluid=True)
+)
 
 # Add controls to build the interaction. The @callback is a decorator to the update_graph() function.
 @callback(
-    Output(component_id='controls-and-graph', component_property='figure')
-    ,Input(component_id='controls-and-radio-item', component_property='value')
+    Output(component_id='graph-placeholder', component_property='figure')
+    ,Input(component_id='my-dmc-radio-item', component_property='value')
 )
 def update_graph(col_chosen): # Takes the Input component_property value as y-axis attribute.
     fig = px.histogram(df, x='continent', y=col_chosen, histfunc='avg')
---
+++ deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Deploying Dash app..."
+
+python3 ./dash_app/app_20.py
