d075f2b924e6ec54513e4e4e90ef664b7dddf71c
spw <pixbin@gmail.com>
2025-12-26 16:10:28 +0000

    Cleaner code

--- doraview/pages/failure.py
+++ doraview/pages/failure.py
@@ -6,7 +6,8 @@ from components.table import reuse_table
 
 dash.register_page(__name__, path='/failures', name='Failure Rate', order=4)
 
-df_apps = pd.read_json('/home/lnx_workspaces/bpp_projects/bpp_module2_project/doraview/data/json/change_fail.json')
+raw_file_path = '/home/lnx_workspaces/bpp_projects/bpp_module2_project/doraview/data/json/change_fail.json'
+df_apps = pd.read_json(raw_file_path, encoding='utf-8', convert_dates=["detected_at"])
 
 layout = dmc.Container([
     dmc.Title("Change Failure Rate"),
